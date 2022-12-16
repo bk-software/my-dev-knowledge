daab3977c10edf0da199e5f52fa997e6a2ffe673
OriGuyUniqueDev <origuydev@gmail.com>
2022-12-16 16:22:34 +0200

    add login page, register page and basic routing main layout header,footer and main without styles/components

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client'
 import {createBrowserRouter,RouterProvider,} from "react-router-dom";
 import App from './App'
 import Layout from './components/layout/Layout';
+import Register from './components/signin_register/Register';
+import SignIn from './components/signin_register/Signin';
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Log in</div>,
+    element:<SignIn />,
+  },
+  {
+    path: "/register",
+    element:<Register />,
   },
   {
     path: "/home",
