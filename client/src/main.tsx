3cab42ec1ae43dc2441678acf75d72b2bc90f5fe
OriGuyUniqueDev <origuydev@gmail.com>
2022-12-16 15:38:17 +0200

    adding basic routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
 import App from './App'
+import Layout from './components/layout/Layout';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Log in</div>,
+  },
+  {
+    path: "/home",
+    element:<Layout />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
