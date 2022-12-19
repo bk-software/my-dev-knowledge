a2a9fd80118a18a87ef24cf4de8243fc242990e2
OriGuyUniqueDev <origuydev@gmail.com>
2022-12-19 22:12:37 +0200

    try to fix the indention in signin component

--- client/src/components/signin_register/Signin.tsx
+++ client/src/components/signin_register/Signin.tsx
@@ -34,13 +34,7 @@ export default function SignIn() {
 				xs={false}
 				sm={4}
 				md={7}
-				sx={{
-					backgroundImage: "url(https://source.unsplash.com/random)",
-					backgroundRepeat: "no-repeat",
-					backgroundColor: (t) => (t.palette.mode === "light" ? t.palette.grey[50] : t.palette.grey[900]),
-					backgroundSize: "cover",
-					backgroundPosition: "center",
-				}}
+				sx={{backgroundImage: "url(https://source.unsplash.com/random)",backgroundRepeat: "no-repeat",backgroundColor: (t) => (t.palette.mode === "light" ? t.palette.grey[50] : t.palette.grey[900]),backgroundSize: "cover",backgroundPosition: "center",}}
 			/>
 			<Grid
 				item
